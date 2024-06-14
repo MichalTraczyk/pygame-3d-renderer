ff687ad7ebf0dedb9a9b284a4e2c92e72828e166
Wahares <piotrekt0000@gmail.com>
2024-06-14 10:57:26 +0200

    more primitives

--- src/MeshSystem/Primitives.py
+++ src/MeshSystem/Primitives.py
@@ -19,7 +19,23 @@ class Primitives:
     @staticmethod
     def generate_pyramid():
         mesh = Mesh(
-            [(0.5, 0, 0), (-0.5, 0, 0), (0, 0, 1.75), (0, 1, 0, 7)],
+            [(0.5, 0, 1.75), (-0.5, 0, 1.75), (0, 0, 0), (0, 1, 1.05)],
             [(0, 2, 1), (0, 1, 3), (1, 2, 3), (0, 3, 2)])
+        mesh.set_color(Color(255, 255, 255))
+        return mesh
+
+    @staticmethod
+    def generate_rotated_pyramid():
+        mesh = Mesh(
+            [(0.5, 0, 0), (-0.5, 0, 0), (0, 0, 1.75), (0, 1, 0.7)],
+            [(0, 2, 1), (0, 1, 3), (1, 2, 3), (0, 3, 2)])
+        mesh.set_color(Color(255, 255, 255))
+        return mesh
+
+    @staticmethod
+    def generate_diamond():
+        mesh = Mesh(
+            [(0.5, 0, 1.75), (-0.5, 0, 1.75), (0, 0, 0), (0, 1, 1.05), (0, -1, 1.05)],
+            [(0, 1, 3), (1, 2, 3), (0, 3, 2), (1, 0, 4), (2, 1, 4), (2, 4, 0)])
         mesh.set_color(Color(255, 255, 255))
         return mesh
