9e8bc62d79d71004672144fb075088f1307bfdc3
Michał Traczyk <mt.michal2002@gmail.com>
2024-06-24 16:55:35 +0200

    Even more docstrings:c

--- src/Math/VectorMath.py
+++ src/Math/VectorMath.py
@@ -7,19 +7,52 @@ from src.MeshSystem.WorldFace import WorldFace
 class VectorMath:
     @staticmethod
     def dot(v1, v2):
+        """
+        Returns dot product of the given two vectors
+        @param v1: 1st vector
+        @type v1: Vector3 or Vector2
+        @param v2: 2nd vector
+        @type v2: Vector3 or Vector2
+        @return: Dot product of two vectors
+        @rtype: float
+        """
         return sum((a * b) for a, b in zip(v1, v2))
 
     @staticmethod
     def length(v):
+        """
+        Returns the length of the given vector
+        @type v: Vector3 or Vector2
+        @param v: Vector to calculate length
+        @rtype: float
+        @return: Length of the vector
+        """
         return math.sqrt(VectorMath.dot(v, v))
 
     # W radianach!!
     @staticmethod
     def unsigned_angle(v1, v2):
+        """
+        Returns the unsigned angle between two vectors
+        @param v1: 1st vector
+        @type v1: Vector3 or Vector2
+        @param v2: 2nd vector
+        @type v2: Vector3 or Vector2
+
+        @rtype: float
+        @return: Unsigned angle between two vectors in radians
+        """
         return math.acos(VectorMath.dot(v1, v2) / (VectorMath.length(v1) * VectorMath.length(v2)))
 
     @staticmethod
     def normalize_vector(vec):
+        """
+        Normalizes the given vector so all lengths sum up to 1
+        @param vec: Vector to normalize
+
+        @rtype: Vector3 or Vector2
+        @return: Normalized vector
+        """
         return vec / VectorMath.length(vec)
 
     @staticmethod
