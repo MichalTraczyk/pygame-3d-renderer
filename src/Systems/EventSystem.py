dcdafd621bbbb6219890f93dc84875dd74f5f6a6
Michał Traczyk <mt.michal2002@gmail.com>
2024-06-24 19:24:18 +0200

    Add docstring to event system

--- src/Systems/EventSystem.py
+++ src/Systems/EventSystem.py
@@ -18,11 +18,16 @@ from pygame.locals import (
 
 
 class EventSystem:
-    onQuit = []
-    pressedDown = []
+    __on_quit = []
+    __pressed_down = []
 
     @staticmethod
     def get_axis():
+        """
+        Returns the active input axis where left and down arrows equal -1
+        @return: Active input axis
+        @rtype: Vector2
+        """
         axis = Vector2(0, 0)
         keys = pygame.key.get_pressed()
         if keys[K_LEFT] or keys[K_a]:
@@ -38,22 +43,35 @@ class EventSystem:
 
     @staticmethod
     def add_on_quit_listener(listener):
-        EventSystem.onQuit.append(listener)
+        """
+        Adds a listener for quit event
+        @param listener: method to call on quit
+        """
+        EventSystem.__on_quit.append(listener)
 
     @staticmethod
-    def fire_on_quit_event():
-        for listener in EventSystem.onQuit:
+    def __fire_on_quit_event():
+        for listener in EventSystem.__on_quit:
             listener()
 
     @staticmethod
     def update():
-        EventSystem.pressedDown.clear()
+        """
+        Refreshes all inputs
+        """
+        EventSystem.__pressed_down.clear()
         for event in pygame.event.get():
             if event.type == KEYDOWN:
-                EventSystem.pressedDown.append(event.key)
+                EventSystem.__pressed_down.append(event.key)
             elif event.type == QUIT:
-                EventSystem.fire_on_quit_event()
+                EventSystem.__fire_on_quit_event()
 
     @staticmethod
     def get_key_down(key):
-        return key in EventSystem.pressedDown
+        """
+        Returns True if the key is pressed
+        @param key: key
+        @return: True if the key is pressed
+        @rtype: bool
+        """
+        return key in EventSystem.__pressed_down
