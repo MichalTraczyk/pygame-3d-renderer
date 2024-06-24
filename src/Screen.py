11a0dd086fcedd449d959f6804a2efdc450f768f
Wahares <piotrekt0000@gmail.com>
2024-06-24 15:22:11 +0200

    Update Screen.py

--- src/Screen.py
+++ src/Screen.py
@@ -26,10 +26,9 @@ class Screen:
         self.running = True
         self.getTicksLastFrame = 0
 
-        canvas = MainCanvas(Vector2(self.resolution[0],self.resolution[1]))
+        canvas = MainCanvas(Vector2(self.resolution[0], self.resolution[1]))
         LightManager.add_change_listener(canvas.hierarchy.lights_changed)
         ModelPool.add_change_listener(canvas.hierarchy.models_changed)
-
 
         mesh = Primitives.generate_box()
         obj = DrawableMesh(Vector3(2, 0, 2))
@@ -40,14 +39,15 @@ class Screen:
         obj.rotate(45)
         obj = DrawableMesh(Vector3(-0.5, -2, 0.8))
         obj.assign_mesh(mesh)
-        obj = DrawableMesh(Vector3(1, -2, 2))
+        obj = DrawableMesh(Vector3(1, 2, 2))
         obj.assign_mesh(Primitives.generate_rotated_pyramid())
+        obj = DrawableMesh(Vector3(0, -2, 1))
+        obj.assign_mesh(Primitives.generate_plane(10, 1))
 
-        #LightManager.register_light(PointLight(Vector3(-1, 0.5, 0.5), 0.9, Color(255, 0, 0)))
-        #LightManager.register_light(PointLight(Vector3(2, 0.5, 0.5), 0.9, Color(0, 255, 0)))
+        LightManager.register_light(PointLight(Vector3(-1, 0.5, 0.5), 0.9, Color(255, 0, 0)))
+        LightManager.register_light(PointLight(Vector3(2, 0.5, 0.5), 0.9, Color(0, 255, 0)))
         LightManager.register_light(SkyboxLight(Vector3(0, 0, 0), 1, Color(255, 0, 0)))
-        LightManager.register_light(DirectionalLight(Vector3(0,0,0),Vector3(1,-1,1), 0.5,Color(255,255,255)))
-
+        LightManager.register_light(DirectionalLight(Vector3(0, 0, 0), Vector3(1, -1, 1), 0.5, Color(255, 255, 255)))
 
         EventSystem.add_on_quit_listener(self.on_quit)
         while self.running:
@@ -64,7 +64,6 @@ class Screen:
 
             # obj.set_position(Vector3(math.sin(self.t / 400), math.cos(self.t / 400)-0.5, 3))
             # obj.rotate(self.deltaTime*200)
-
             if Drawer.is_dirty:
                 self.screen.fill((0, 0, 0))
                 Drawer.draw(self.screen, self.camera)
