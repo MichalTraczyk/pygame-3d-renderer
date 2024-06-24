71a2d5f955488b57258c8d4b4f8bca2d5039a7d9
Wahares <piotrekt0000@gmail.com>
2024-06-24 14:51:31 +0200

    primitive plane generation

--- src/MeshSystem/Primitives.py
+++ src/MeshSystem/Primitives.py
@@ -39,3 +39,28 @@ class Primitives:
             [(0, 1, 3), (1, 2, 3), (0, 3, 2), (1, 0, 4), (2, 1, 4), (2, 4, 0)])
         mesh.set_color(Color(255, 255, 255))
         return mesh
+
+    @staticmethod
+    def generate_plane(size: int, space: float = 1):
+        offset = -size * space / 2
+        if size <= 0 or space <= 0:
+            raise ValueError('Size and space can not be negative or equal to zero!')
+        faces = []
+        for i in range(size * (size+1)-1):
+            faces.append((i, i + size + 1, i + 1))
+            faces.append((i + size + 1, i + size + 2, i + 1))
+        # for z in range(size - 1):
+        #   for x in range(size - 1):
+        #      faces.append((x, (z + 1) * size + x, x + 1))
+        #     faces.append(((z + 1) * size + x, (z + 1) * size + x + 1, x + 1))
+
+        size += 1
+        vertices = []
+        for i in range(size * size):
+            x = i % size
+            z = int(i / size)
+            vertices.append((space * x + offset, 0, space * z + offset))
+
+        mesh = Mesh(vertices, faces)
+        mesh.set_color(Color(255, 255, 255))
+        return mesh
